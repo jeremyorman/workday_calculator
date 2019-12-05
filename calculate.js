5a864d271a010a902e56f7cdedf28fa40360681c
jeremyorman <jeremyorman@outlook.com>
2019-12-04 20:58:21 -0600

    reformatted code

--- calculate.js
+++ calculate.js
@@ -1,46 +1,46 @@
-const electron = require('electron')
-const form = document.querySelector('form')
+const electron = require('electron');
+const form = document.querySelector('form');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const arrival = document.querySelector('#arrival').value
-    const lunch = document.querySelector('#lunch').value
-    const hoursWorked = document.querySelector('#hoursWorked').value
-    const outputDiv = document.querySelector('#output')
-    const output = document.querySelector('#output h2')
+    e.preventDefault();
+    const arrival = document.querySelector('#arrival').value;
+    const lunch = document.querySelector('#lunch').value;
+    const hoursWorked = document.querySelector('#hoursWorked').value;
+    const outputDiv = document.querySelector('#output');
+    const output = document.querySelector('#output h2');
     
-    let timeArray = arrival.split(':')
+    let timeArray = arrival.split(':');
     
-    let hour = timeArray[0]
-    let workDuration = ((parseInt(timeArray[1]) + parseInt(lunch)) / 60) + parseFloat(hoursWorked)
-    let hours = Math.floor(workDuration)
-    let minutes = Math.round((workDuration % 1) * 60)
-    let period = 'am'
+    let hour = timeArray[0];
+    let workDuration = ((parseInt(timeArray[1]) + parseInt(lunch)) / 60) + parseFloat(hoursWorked);
+    let hours = Math.floor(workDuration);
+    let minutes = Math.round((workDuration % 1) * 60);
+    let period = 'am';
     
     if (hours >= 24) {
-        output.textContent = 'Look at Mr. Worksalot over here'
+        output.textContent = 'Look at Mr. Worksalot over here';
         return
     }
     
     if (hour > 12) {
         period = 'pm'
-    
+        
     }
     for (let i = 1; i <= hours; i++) {
-        hour++
+        hour++;
         if (hour > 12) {
-            hour = 1
+            hour = 1;
             period === 'am' ? period = 'pm' : period = 'am'
         }
-    
+        
     }
     if (minutes < 10) {
         minutes = '0' + minutes.toString()
-    
+        
     }
     
-    let departTime = hour + ':' + minutes + ' ' + period
-    outputDiv.classList.remove('hidden')
+    let departTime = hour + ':' + minutes + ' ' + period;
+    outputDiv.classList.remove('hidden');
     output.textContent = 'Go home at ' + departTime
     
-})
+});
--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const {app, BrowserWindow, Menu} = require('electron');
 
-let win
+let win;
 
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'development';
+
 // process.env.NODE_ENV = 'production'
 
-function createWindow () {
+function createWindow() {
     win = new BrowserWindow({
         width: 500,
         height: 400,
         webPreferences: {
             nodeIntegration: true
         }
-    })
+    });
     
-    win.loadFile('index.html')
+    win.loadFile('index.html');
     
     win.on('closed', () => {
         win = null
@@ -22,20 +23,20 @@ function createWindow () {
 }
 
 app.on('ready', () => {
-    createWindow()
+    createWindow();
     if (process.env.NODE_ENV === 'production') {
         Menu.setApplicationMenu(null)
     }
-})
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', () => {
     if (win === null) {
         createWindow()
     }
-})
+});
